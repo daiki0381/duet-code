db77690ceab60529e4ef1dd68afe4ccac6e9ada1
daiki0381 <daiki03810218@gmail.com>
2022-11-20 17:46:35 +0900

    ✨ プライバシーポリシーの細部実装

---
+++ frontend/app/src/components/templates/PrivacyPolicy.tsx
@@ -0,0 +1,87 @@
+import type { NextPage } from 'next'
+
+const PrivacyPolicy: NextPage = () => {
+  return (
+    <div className="flex-1 px-[120px] py-[50px]">
+      <h1 className="mb-5 text-2xl font-bold text-black">
+        プライバシーポリシー
+      </h1>
+      <p className="mb-[30px] text-sm text-black">
+        本ウェブサイト上で提供するサービス（以下,「本サービス」といいます。）における、ユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
+      </p>
+      <h2 className="mb-5 text-xl font-semibold text-black">
+        第1条（個人情報）
+      </h2>
+      <p className="mb-[30px] text-sm text-black">
+        「個人情報」とは、個人情報保護法にいう「個人情報」を指すものとし、特定の個人を識別できる情報（個人識別情報）を指します。
+      </p>
+      <h2 className="mb-5 text-xl font-semibold text-black">
+        第2条（個人情報の収集方法）
+      </h2>
+      <p className="mb-[30px] text-sm text-black">
+        本サービスは、ユーザーが利用登録をする際にユーザーのGitHubアカウント情報を取得します。
+      </p>
+      <h2 className="mb-5 text-xl font-semibold text-black">
+        第3条（個人情報を収集・利用する目的）
+      </h2>
+      <div className="mb-[30px] text-sm text-black">
+        <p className="mb-5">
+          本サービスが個人情報を収集・利用する目的は、以下のとおりです。
+        </p>
+        <ol className="list-decimal pl-10">
+          <li className="mb-[10px]">本サービスの提供・運営のため</li>
+          <li className="mb-[10px]">
+            ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）
+          </li>
+          <li className="mb-[10px]">
+            メンテナンス、重要なお知らせなど必要に応じたご連絡のため
+          </li>
+          <li className="mb-[10px]">
+            利用規約に違反したユーザーや、不正・不当な目的でサービスを利用しようとするユーザーの特定をし、ご利用をお断りするため
+          </li>
+          <li className="mb-[10px]">
+            ユーザーにご自身の登録情報の閲覧や変更、削除、ご利用状況の閲覧を行っていただくため
+          </li>
+          <li>上記の利用目的に付随する目的</li>
+        </ol>
+      </div>
+      <h2 className="mb-5 text-xl font-semibold text-black">
+        第4条（利用目的の変更）
+      </h2>
+      <div className="mb-[30px] text-sm text-black">
+        <ol className="list-decimal pl-10">
+          <li className="mb-[10px]">
+            本サービスは、利用目的が変更前と関連性を有すると合理的に認められる場合に限り、個人情報の利用目的を変更するものとします。
+          </li>
+          <li>
+            利用目的の変更を行った場合には、変更後の目的について、本サービス所定の方法により、ユーザーに通知し、または本ウェブサイト上に公表するものとします。
+          </li>
+        </ol>
+      </div>
+      <h2 className="mb-5 text-xl font-semibold text-black">
+        第5条（個人情報の第三者提供）
+      </h2>
+      <p className="mb-[30px] text-sm text-black">
+        本サービスはユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。
+      </p>
+      <h2 className="mb-5 text-xl font-semibold text-black">
+        第6条（プライバシーポリシーの変更）
+      </h2>
+      <div className="mb-[30px] text-sm text-black">
+        <p className="mb-5">
+          本サービスが個人情報を収集・利用する目的は、以下のとおりです。
+        </p>
+        <ol className="list-decimal pl-10">
+          <li className="mb-[10px]">
+            本ポリシーの内容は、法令その他本ポリシーに別段の定めのある事項を除いて、ユーザーに通知することなく、変更することができるものとします。
+          </li>
+          <li>
+            本サービスが別途定める場合を除いて、変更後のプライバシーポリシーは、本ウェブサイトに掲載したときから効力を生じるものとします。
+          </li>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
--- frontend/app/src/pages/pp.tsx
+++ frontend/app/src/pages/pp.tsx
@@ -1,7 +1,119 @@
 import type { NextPage } from 'next'
+import {
+  withAuthUser,
+  withAuthUserTokenSSR,
+  useAuthUser,
+} from 'next-firebase-auth'
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query'
+import { userApi, notificationApi } from '@/api/custom-instance'
+import PreLoginHeader from '@/components/organisms/PreLoginHeader'
+import PostLoginHeader from '@/components/organisms/PostLoginHeader'
+import PrivacyPolicy from '@/components/templates/PrivacyPolicy'
+import PreLoginFooter from '@/components/organisms/PreLoginFooter'
+import PostLoginFooter from '@/components/organisms/PostLoginFooter'
 
-const PrivacyPolicy: NextPage = () => {
-  return <div>プライバシーポリシー</div>
+const Pp: NextPage<any> = () => {
+  const AuthUser = useAuthUser()
+  const avatar = AuthUser.photoURL
+
+  const { data: userId } = useQuery(
+    ['userId'],
+    async () => {
+      const token = await AuthUser.getIdToken()
+      if (token !== null) {
+        const { data } = await userApi.getCurrentUserId({
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        return data
+      }
+    },
+    {
+      enabled: AuthUser.id !== null,
+    },
+  )
+
+  const { data: notifications } = useQuery(
+    ['notifications'],
+    async () => {
+      const token = await AuthUser.getIdToken()
+      if (token !== null) {
+        const { data } =
+          await await notificationApi.getCurrentUserNotifications({
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+        return data
+      }
+    },
+    {
+      enabled: AuthUser.id !== null,
+    },
+  )
+
+  const currentUserId = userId !== undefined ? userId : 0
+  const reviewNotifications = notifications !== undefined ? notifications : []
+  const badgeContent = reviewNotifications.filter(
+    (notification) => notification.checked === false,
+  ).length
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      {avatar !== null ? (
+        <>
+          <PostLoginHeader
+            avatar={avatar}
+            userId={currentUserId}
+            notifications={reviewNotifications}
+            badgeContent={badgeContent}
+          />
+          <PrivacyPolicy />
+          <PostLoginFooter />
+        </>
+      ) : (
+        <>
+          <PreLoginHeader />
+          <PrivacyPolicy />
+          <PreLoginFooter />
+        </>
+      )}
+    </div>
+  )
 }
 
-export default PrivacyPolicy
+export const getServerSideProps = withAuthUserTokenSSR()(
+  async ({ AuthUser }) => {
+    const queryClient = new QueryClient()
+    const token = await AuthUser.getIdToken()
+
+    if (token !== null) {
+      await queryClient.prefetchQuery(['userId'], async () => {
+        const { data } = await userApi.getCurrentUserId({
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        return data
+      })
+
+      await queryClient.prefetchQuery(['notifications'], async () => {
+        const { data } = await notificationApi.getCurrentUserNotifications({
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        return data
+      })
+    }
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+      },
+    }
+  },
+)
+
+export default withAuthUser()(Pp)
