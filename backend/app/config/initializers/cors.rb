f3ea7e9432ed135416a904138b9c925ccdf55389
daiki0381 <daiki03810218@gmail.com>
2022-11-21 22:56:55 +0900

    🚀 RailsAPIをFly.ioにデプロイ

--- backend/app/config/initializers/cors.rb
+++ backend/app/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8000'
+    origins 'http://localhost:8000', 'https://duet-code.vercel.app'
 
     resource '*',
              headers: :any,
