bc84276838f6a5a22e1241569680e3afb5080672
daiki0381 <daiki03810218@gmail.com>
2022-11-23 22:33:55 +0900

    💄 ログイン後/マイページのレスポンシブ対応

--- frontend/app/src/components/templates/UserDetails.tsx
+++ frontend/app/src/components/templates/UserDetails.tsx
@@ -79,13 +79,13 @@ const UserDetails: NextPage<Props> = ({
           </IconButton>
         </a>
       </div>
-      <div className="mx-[180px] flex">
+      <div className="mx-auto flex  px-4 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px]">
         <p
           onClick={clickThanks}
           className={
             status === 'thanks'
-              ? 'mr-5 cursor-pointer border-b-2 border-blue pb-[10px] text-lg font-semibold text-blue hover:opacity-70'
-              : 'mr-5 cursor-pointer text-lg text-black hover:opacity-70'
+              ? 'mr-[10px] cursor-pointer border-b-2 border-blue pb-[10px] text-sm font-semibold text-blue hover:opacity-70 sm:mr-5 sm:text-lg'
+              : 'mr-[10px] cursor-pointer text-sm text-black hover:opacity-70 sm:mr-5 sm:text-lg'
           }
         >
           お礼
@@ -94,8 +94,8 @@ const UserDetails: NextPage<Props> = ({
           onClick={clickFeedback}
           className={
             status === 'feedback'
-              ? 'mr-5 cursor-pointer border-b-2 border-blue pb-[10px] text-lg font-semibold text-blue hover:opacity-70'
-              : 'mr-5 cursor-pointer text-lg text-black hover:opacity-70'
+              ? 'mr-[10px] cursor-pointer border-b-2 border-blue pb-[10px] text-sm font-semibold text-blue hover:opacity-70 sm:mr-5 sm:text-lg'
+              : 'mr-[10px] cursor-pointer text-sm text-black hover:opacity-70 sm:mr-5 sm:text-lg'
           }
         >
           フィードバック
@@ -104,8 +104,8 @@ const UserDetails: NextPage<Props> = ({
           onClick={clickWanted}
           className={
             status === 'wanted'
-              ? 'mr-5 cursor-pointer border-b-2 border-blue pb-[10px] text-lg font-semibold text-blue hover:opacity-70'
-              : 'mr-5 cursor-pointer text-lg text-black hover:opacity-70'
+              ? 'mr-[10px] cursor-pointer border-b-2 border-blue pb-[10px] text-sm font-semibold text-blue hover:opacity-70 sm:mr-5 sm:text-lg'
+              : 'mr-[10px] cursor-pointer text-sm text-black hover:opacity-70 sm:mr-5 sm:text-lg'
           }
         >
           募集した一覧
@@ -114,153 +114,155 @@ const UserDetails: NextPage<Props> = ({
           onClick={clickAccepted}
           className={
             status === 'accepted'
-              ? 'mr-5 cursor-pointer border-b-2 border-blue pb-[10px] text-lg font-semibold text-blue hover:opacity-70'
-              : 'mr-5 cursor-pointer text-lg text-black hover:opacity-70'
+              ? 'cursor-pointer border-b-2 border-blue pb-[10px] text-sm font-semibold text-blue hover:opacity-70 sm:text-lg'
+              : 'cursor-pointer text-sm text-black hover:opacity-70 sm:text-lg'
           }
         >
           承諾した一覧
         </p>
       </div>
-      <div className="bg-post-login-light-blue">
-        {(() => {
-          if (status === 'thanks' && thanksList.length === 0) {
-            return (
-              <div className="pt-[20px] pb-[50px] text-center">
-                <Image
-                  src="/user-details-thanks.svg"
-                  width={350}
-                  height={350}
-                  alt="お礼が無い場合のイラスト"
-                />
-                <p className="text-xl text-black">お礼はまだありません</p>
-              </div>
-            )
-          } else if (status === 'feedback' && feedbackList.length === 0) {
-            return (
-              <div className="pt-[20px] pb-[50px] text-center">
-                <Image
-                  src="/user-details-feedback.svg"
-                  width={350}
-                  height={350}
-                  alt="フィードバックが無い場合のイラスト"
-                />
-                <p className="text-xl text-black">
-                  フィードバックはまだありません
-                </p>
-              </div>
-            )
-          } else if (status === 'wanted' && wantedReviews.length === 0) {
-            return (
-              <div className="pt-[20px] pb-[50px] text-center">
-                <Image
-                  src="/user-details-wanted.svg"
-                  width={350}
-                  height={350}
-                  alt="募集した一覧が無い場合のイラスト"
-                />
-                <p className="text-xl text-black">
-                  募集した一覧はまだありません
-                </p>
-              </div>
-            )
-          } else if (status === 'accepted' && acceptedReviews.length === 0) {
-            return (
-              <div className="pt-[20px] pb-[50px] text-center">
-                <Image
-                  src="/user-details-accepted.svg"
-                  width={350}
-                  height={350}
-                  alt="承諾した一覧が無い場合のイラスト"
-                />
-                <p className="text-xl text-black">
-                  承諾した一覧はまだありません
-                </p>
-              </div>
-            )
-          } else if (status === 'thanks' && thanksList.length !== 0) {
-            return (
-              <div className="px-[120px] pt-[50px]">
-                {thanksList.map((thanks: any) => {
-                  return (
-                    <div key={thanks.id} className="pb-[50px]">
-                      <ThanksOrFeedbackCard
-                        name={thanks.name}
-                        avatar={thanks.avatar}
-                        message={thanks.message}
-                        onClick={() => goToPostsDetails(thanks.id)}
-                      />
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          } else if (status === 'feedback' && feedbackList.length !== 0) {
-            return (
-              <div className="px-[120px] pt-[50px]">
-                {feedbackList.map((feedback: any) => {
-                  return (
-                    <div key={feedback.id} className="pb-[50px]">
-                      <ThanksOrFeedbackCard
-                        name={feedback.name}
-                        avatar={feedback.avatar}
-                        message={feedback.message}
-                        onClick={() => goToPostsDetails(feedback.id)}
-                      />
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          } else if (status === 'wanted' && wantedReviews.length !== 0) {
-            return (
-              <div className="px-[120px] pt-[50px]">
-                <div className="flex flex-wrap">
-                  {wantedReviews.map((wantedReview: any) => {
+      <div className="bg-post-login-light-blue md:min-h-[615px] lg:min-h-[810px]">
+        <div className="mx-auto px-4 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px]">
+          {(() => {
+            if (status === 'thanks' && thanksList.length === 0) {
+              return (
+                <div className="pt-[20px] pb-[50px] text-center">
+                  <Image
+                    src="/user-details-thanks.svg"
+                    width={350}
+                    height={350}
+                    alt="お礼が無い場合のイラスト"
+                  />
+                  <p className="text-xl text-black">お礼はまだありません</p>
+                </div>
+              )
+            } else if (status === 'feedback' && feedbackList.length === 0) {
+              return (
+                <div className="pt-[20px] pb-[50px] text-center">
+                  <Image
+                    src="/user-details-feedback.svg"
+                    width={350}
+                    height={350}
+                    alt="フィードバックが無い場合のイラスト"
+                  />
+                  <p className="text-xl text-black">
+                    フィードバックはまだありません
+                  </p>
+                </div>
+              )
+            } else if (status === 'wanted' && wantedReviews.length === 0) {
+              return (
+                <div className="pt-[20px] pb-[50px] text-center">
+                  <Image
+                    src="/user-details-wanted.svg"
+                    width={350}
+                    height={350}
+                    alt="募集した一覧が無い場合のイラスト"
+                  />
+                  <p className="text-xl text-black">
+                    募集した一覧はまだありません
+                  </p>
+                </div>
+              )
+            } else if (status === 'accepted' && acceptedReviews.length === 0) {
+              return (
+                <div className="pt-[20px] pb-[50px] text-center">
+                  <Image
+                    src="/user-details-accepted.svg"
+                    width={350}
+                    height={350}
+                    alt="承諾した一覧が無い場合のイラスト"
+                  />
+                  <p className="text-xl text-black">
+                    承諾した一覧はまだありません
+                  </p>
+                </div>
+              )
+            } else if (status === 'thanks' && thanksList.length !== 0) {
+              return (
+                <div className="pt-[50px]">
+                  {thanksList.map((thanks: any) => {
                     return (
-                      <div
-                        key={wantedReview.id}
-                        className="mb-[50px] w-[33.3%]"
-                      >
-                        <ReviewCard
-                          reviewId={wantedReview.id}
-                          title={wantedReview.title}
-                          languages={wantedReview.languages}
-                          revieweeName={wantedReview.reviewee.name}
-                          revieweeAvatar={wantedReview.reviewee.avatar}
-                          createdAt={wantedReview.created_at}
+                      <div key={thanks.id} className="pb-[50px]">
+                        <ThanksOrFeedbackCard
+                          name={thanks.name}
+                          avatar={thanks.avatar}
+                          message={thanks.message}
+                          onClick={() => goToPostsDetails(thanks.id)}
                         />
                       </div>
                     )
                   })}
                 </div>
-              </div>
-            )
-          } else if (status === 'accepted' && acceptedReviews.length !== 0) {
-            return (
-              <div className="px-[120px] pt-[50px]">
-                <div className="flex flex-wrap">
-                  {acceptedReviews.map((acceptedReview: any) => {
+              )
+            } else if (status === 'feedback' && feedbackList.length !== 0) {
+              return (
+                <div className="pt-[50px]">
+                  {feedbackList.map((feedback: any) => {
                     return (
-                      <div
-                        key={acceptedReview.id}
-                        className="mb-[50px] w-[33.3%]"
-                      >
-                        <ReviewCard
-                          reviewId={acceptedReview.id}
-                          title={acceptedReview.title}
-                          languages={acceptedReview.languages}
-                          revieweeName={acceptedReview.reviewee.name}
-                          revieweeAvatar={acceptedReview.reviewee.avatar}
-                          createdAt={acceptedReview.created_at}
+                      <div key={feedback.id} className="pb-[50px]">
+                        <ThanksOrFeedbackCard
+                          name={feedback.name}
+                          avatar={feedback.avatar}
+                          message={feedback.message}
+                          onClick={() => goToPostsDetails(feedback.id)}
                         />
                       </div>
                     )
                   })}
                 </div>
-              </div>
-            )
-          }
-        })()}
+              )
+            } else if (status === 'wanted' && wantedReviews.length !== 0) {
+              return (
+                <div className="pt-[50px]">
+                  <div className="flex flex-wrap">
+                    {wantedReviews.map((wantedReview: any) => {
+                      return (
+                        <div
+                          key={wantedReview.id}
+                          className="mb-[50px] w-full lg:w-1/2  xl:w-1/3"
+                        >
+                          <ReviewCard
+                            reviewId={wantedReview.id}
+                            title={wantedReview.title}
+                            languages={wantedReview.languages}
+                            revieweeName={wantedReview.reviewee.name}
+                            revieweeAvatar={wantedReview.reviewee.avatar}
+                            createdAt={wantedReview.created_at}
+                          />
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              )
+            } else if (status === 'accepted' && acceptedReviews.length !== 0) {
+              return (
+                <div className="pt-[50px]">
+                  <div className="flex flex-wrap">
+                    {acceptedReviews.map((acceptedReview: any) => {
+                      return (
+                        <div
+                          key={acceptedReview.id}
+                          className="mb-[50px] w-full lg:w-1/2  xl:w-1/3"
+                        >
+                          <ReviewCard
+                            reviewId={acceptedReview.id}
+                            title={acceptedReview.title}
+                            languages={acceptedReview.languages}
+                            revieweeName={acceptedReview.reviewee.name}
+                            revieweeAvatar={acceptedReview.reviewee.avatar}
+                            createdAt={acceptedReview.created_at}
+                          />
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              )
+            }
+          })()}
+        </div>
       </div>
     </div>
   )
