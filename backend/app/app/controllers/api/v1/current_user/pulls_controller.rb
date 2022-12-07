eff25e728245a045cd77930fec7fd4dedf8a20ab
daiki0381 <daiki03810218@gmail.com>
2022-12-07 14:48:00 +0900

    🐛 他人のPRが表示されるバグの解消

--- backend/app/app/controllers/api/v1/current_user/pulls_controller.rb
+++ backend/app/app/controllers/api/v1/current_user/pulls_controller.rb
@@ -17,8 +17,8 @@ module Api
           client = Octokit::Client.new(access_token: github_access_token)
           repos = client.repos(name).map(&:name)
           pulls = Parallel.map(repos, in_threads: 10) do |repo|
-            client.pulls("#{name}/#{repo}").map do |pull|
-              { title: "#{pull.head.repo.name}/#{pull.title}", url: pull.html_url }
+            client.pulls("#{name}/#{repo}").filter_map do |pull|
+              { title: "#{pull.head.repo.name}/#{pull.title}", url: pull.html_url } if pull.user.login == name
             end
           end.flatten
           render json: pulls, status: :ok
