af048e4d155e392375cbc51d6233420da6b0491d
daiki0381 <daiki03810218@gmail.com>
2022-12-10 23:30:39 +0900

    ♻️ レビューリクエストをリファクタリング

--- backend/app/app/controllers/api/v1/request_reviews_controller.rb
+++ backend/app/app/controllers/api/v1/request_reviews_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'octokit'
-
 module Api
   module V1
     class RequestReviewsController < ApplicationController
@@ -9,15 +7,7 @@ module Api
         @review = Review.find(params[:review_id])
         @reviewee = User.find(@review.reviewee_id)
         @reviewer = User.find(@review.reviewer_id)
-        reviewee = Octokit::Client.new(access_token: @reviewee.github_access_token)
-        reviewer = Octokit::Client.new(access_token: @reviewer.github_access_token)
-        repository = @review.pull_request_url.split('/')[4]
-        name_and_repository = "#{@reviewee.name}/#{repository}"
-        reviewee.add_collaborator(name_and_repository, @reviewer.name)
-        repository_invitations = reviewee.repository_invitations(name_and_repository)
-        reviewer.accept_repository_invitation(repository_invitations[0].id) unless repository_invitations.empty?
-        pull_request_number = @review.pull_request_url.split('/').last
-        reviewee.request_pull_request_review(name_and_repository, pull_request_number, reviewers: [@reviewer.name])
+        @review.request_review(@reviewee, @reviewer)
         render json: { status_code: 201, status: 'created' }, status: :created
       end
     end
--- backend/app/app/models/review.rb
+++ backend/app/app/models/review.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
+require 'octokit'
+
 class Review < ApplicationRecord
   belongs_to :reviewee, class_name: 'User', optional: true
   belongs_to :reviewer, class_name: 'User', optional: true
   has_many :review_languages, dependent: :destroy
   has_many :languages, through: :review_languages
   has_many :notifications, dependent: :destroy
+
+  def request_review(reviewee, reviewer)
+    reviewee_client = Octokit::Client.new(access_token: reviewee.github_access_token)
+    reviewer_client = Octokit::Client.new(access_token: reviewer.github_access_token)
+    repository = pull_request_url.split('/')[4]
+    name_and_repository = "#{reviewee.name}/#{repository}"
+    reviewee_client.add_collaborator(name_and_repository, reviewer.name)
+    repository_invitations = reviewee_client.repository_invitations(name_and_repository)
+    reviewer_client.accept_repository_invitation(repository_invitations[0].id) unless repository_invitations.empty?
+    pull_request_number = pull_request_url.split('/').last
+    reviewee_client.request_pull_request_review(name_and_repository, pull_request_number, reviewers: [reviewer.name])
+  end
 end
