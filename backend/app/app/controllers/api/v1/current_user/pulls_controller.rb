65793993f399517b6450bc4a43bd34194b92a0fb
daiki0381 <daiki03810218@gmail.com>
2023-06-05 05:12:32 +0900

    ♻️ PullsControllerのindexアクションをリファクタリング

--- backend/app/app/controllers/api/v1/current_user/pulls_controller.rb
+++ backend/app/app/controllers/api/v1/current_user/pulls_controller.rb
@@ -11,14 +11,20 @@ module Api
           @user = User.find(user_id)
           name = @user.name
           github_access_token = @user.github_access_token
+          pulls = fetch_pulls(name, github_access_token)
+          render json: pulls, status: :ok
+        end
+
+        private
+
+        def fetch_pulls(name, github_access_token)
           client = Octokit::Client.new(access_token: github_access_token)
           repos = client.repos(name).map(&:name)
-          pulls = Parallel.map(repos, in_threads: 10) do |repo|
+          Parallel.map(repos, in_threads: 10) do |repo|
             client.pulls("#{name}/#{repo}").filter_map do |pull|
               { title: "#{pull.head.repo.name}/#{pull.title}", url: pull.html_url } if pull.user.login == name
             end
           end.flatten
-          render json: pulls, status: :ok
         end
       end
     end
